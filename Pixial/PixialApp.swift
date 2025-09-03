fb2bce48190244ec033bd316491e469a498ff6e5
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-09-03 09:45:31 +0800

    Initial Commit

---
+++ Pixial/PixialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixialApp.swift
+//  Pixial
+//
+//  Created by Jourdese Palacio on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PixialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
